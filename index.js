ff3126e49c03904d10696dc6e5b556aadd5abd81
nimrod kipngetich <nimrod.kipngetich@student.moringaschool.com>
2025-06-20 14:09:14 +0300

    update the latest commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+const drivers =['Bobby','Sammy','sally','Annette','Sarah','Bobby']
+function findMatching(drivers, name) {
+ return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+ 
+}
+// console.log(findMatching(drivers, 'Bobby')) 
+
+function fuzzyMatch(drivers, letters){
+
+    return drivers.filter(driver =>
+        driver.toLowerCase().startsWith(letters.toLowerCase())
+      );
+    }
+
+
+    function matchName(drivers, name) {
+        return drivers.filter(driver => driver.name === name);
+      }
+    
